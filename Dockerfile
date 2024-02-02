9824ba9caac6c3086c3ab118150183ad65cf5471
kameshrsk <kamesh030301@gmail.com>
2024-02-02 18:25:56 +0530

    Front End Added

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3.11
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-CMD python api.py
+EXPOSE 8000
+CMD ["uvicorn", "api:app", "--host", "0.0.0.0", "--port", "8000"]
--- api.py
+++ api.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
-
 from banknote import BankNote
-
 import pickle
 
 model_dict=pickle.load(open("random_forest_classifier.pkl", "rb"))
 
 classifier=model_dict['model']
 app=FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get('/')
 def home():
@@ -22,16 +23,14 @@ def predict_note(data:BankNote):
     curtosis=data["curtosis"]
     entropy=data["entropy"]
 
-
     prediction=classifier.predict([[variance, skewness, curtosis, entropy]])
 
     if prediction[0]>0.5:
         prediction="Fake Note"
-
     else:
         prediction="Bank Note"
 
-    return {"The note is predicted to be a:":prediction}
+    return {"prediction":prediction}
 
 if __name__=="__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
